03429349ad03b8449121148c7fbb32956f555ca5
sa-kevin <kevin@s-arcana.co.jp>
2024-07-01 18:48:56 +0900

    fix: update the tables

--- database/migrations/2024_06_28_092147_create_books_table.php
+++ database/migrations/2024_06_28_092147_create_books_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->text('description')->nullable();
+            $table->string('isbn')->unique()->nullable();
+            $table->date('published_at')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2024_06_28_092329_create_loans_table.php
+++ database/migrations/2024_06_28_092329_create_loans_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('loans', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('book_id')->constrained()->onDelete('cascade');
+            $table->timestamp('loaned_at');
+            $table->date('due_date');
+            $table->timestamp('returned_at')->nullable();
             $table->timestamps();
         });
     }
