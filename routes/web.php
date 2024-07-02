648c9a9665070529c47c306383397b09538f4cbe
sa-kevin <kevin@s-arcana.co.jp>
2024-07-02 17:19:20 +0900

    add routes for book and Loan

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\LoanController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +24,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'verified'])->group(function (){
+    Route::resource('books', BookController::class);
+    Route::resource('loans', LoanController::class);
 });
 
 require __DIR__.'/auth.php';
