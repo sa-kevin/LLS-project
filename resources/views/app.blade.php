b27799a21de11d64b1545a91bae56b5c621d0361
sa-kevin <kevin@s-arcana.co.jp>
2024-07-03 19:25:36 +0900

    change favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" type="image/png" href="/images/logo_bt.png">
+        <title inertia>{{ config('app.name', 'Tech Book') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
