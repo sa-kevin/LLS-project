1d0d9eac547d584d86a53df32d4b65494517ec7d
sa-kevin <kevin@s-arcana.co.jp>
2024-07-04 16:09:55 +0900

    add function to render book loan details

--- resources/js/Components/Book/BookItem.jsx
+++ resources/js/Components/Book/BookItem.jsx
@@ -1,21 +1,42 @@
-import { Link } from '@inertiajs/react';
+import { useState } from 'react';
+import BookCard from './BookCard';
 
 export default function BookItem({ book }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleRentOrWait = () => {
+    console.log('Rent or wait for:', book.title);
+  };
+  const handleAddToWishlist = () => {
+    console.log('add to wishlist:', book.title);
+  };
+
   return (
-    <div className="bg-white shadow-sm border border-gray-200 rounded-lg p-4 flex justify-between items-center">
-      <div>
-        <div className="flex items-center mb-2">
-          <h3 className="font-bold text-xl mr-4">{book.title}</h3>
-          <p className="text-gray-700 text-sm">{book.author}</p>
+    <>
+      <div className="bg-white shadow-sm border border-gray-200 rounded-lg p-4 flex justify-between items-center">
+        <div>
+          <div className="flex items-center mb-2">
+            <h3 className="font-bold text-xl mr-4">{book.title}</h3>
+            <p className="text-gray-700 text-sm">{book.author}</p>
+          </div>
+          <p className="text-gray-500 text-sm">ISBN: {book.isbn} </p>
         </div>
-        <p className="text-gray-500 text-sm">ISBN: {book.isbn} </p>
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4"
+          //href={route('books.show', book.id)}
+        >
+          View Details
+        </button>
       </div>
-      <Link
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4"
-        href={route('books.show', book.id)}
-      >
-        View Details
-      </Link>
-    </div>
+      {isModalOpen && (
+        <BookCard
+          book={book}
+          onClose={() => setIsModalOpen(false)}
+          onRentOrWait={handleRentOrWait}
+          onAddToWishlist={handleAddToWishlist}
+        />
+      )}
+    </>
   );
 }
