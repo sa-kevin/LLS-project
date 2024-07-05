0ca1934165023f3b5ee4394bb42b4b491dcead33
sa-kevin <kevin@s-arcana.co.jp>
2024-07-05 21:27:27 +0900

    add wishlist and user card

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,21 +1,32 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import UserCard from '../Components/User/UserCard';
+import Wishlist from '../Components/User/Wishlist';
 
-export default function Dashboard({ auth }) {
-    return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
-        >
-            <Head title="Dashboard" />
+export default function Dashboard({ auth, wishlist }) {
+  console.log('Wishlist Items:', wishlist);
+  return (
+    <AuthenticatedLayout
+      user={auth.user}
+      header={
+        <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+          Dashboard
+        </h2>
+      }
+    >
+      <Head title="Dashboard" />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
-                    </div>
-                </div>
+      <div className="py-12">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div className="p-6 text-gray-900 text-center ">Welcome User!</div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <UserCard user={auth.user} />
+              <Wishlist wishlists={wishlist} />
             </div>
-        </AuthenticatedLayout>
-    );
+          </div>
+        </div>
+      </div>
+    </AuthenticatedLayout>
+  );
 }
