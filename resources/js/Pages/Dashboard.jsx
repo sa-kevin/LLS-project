7f7c31c145e29bd2e5c5948f7a782b63d3d4d468
sa-kevin <kevin@s-arcana.co.jp>
2024-07-08 12:17:26 +0900

    feat: function to display flash with timer

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,10 +1,24 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import { Head, usePage } from '@inertiajs/react';
 import UserCard from '../Components/User/UserCard';
 import Wishlist from '../Components/User/Wishlist';
+import { useEffect, useState } from 'react';
 
 export default function Dashboard({ auth, wishlist }) {
   console.log('Wishlist Items:', wishlist);
+
+  const { flash } = usePage().props;
+  const [showFlash, setShowFlash] = useState(false);
+
+  useEffect(() => {
+    if (flash && flash.success) {
+      setShowFlash(true);
+      const timer = setTimeout(() => {
+        setShowFlash(false);
+      }, 2000);
+      return () => clearTimeout(timer);
+    }
+  }, [flash]);
   return (
     <AuthenticatedLayout
       user={auth.user}
@@ -15,6 +29,17 @@ export default function Dashboard({ auth, wishlist }) {
       }
     >
       <Head title="Dashboard" />
+
+      {showFlash && flash && flash.success && (
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-4">
+          <div
+            className="bg-green-100 border-green-400 text-green-700 px-4 py-3 rounded relative"
+            role="alert"
+          >
+            <span className="block sm:inline">{flash.success}</span>
+          </div>
+        </div>
+      )}
 
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
