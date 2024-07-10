448e1bf84431a1ce3ab1224888842eaefba93c7e
sa-kevin <kevin@s-arcana.co.jp>
2024-07-10 14:53:44 +0900

    modify flash shareing setting

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -34,6 +34,8 @@ class HandleInertiaRequests extends Middleware
             'auth' => [
                 'user' => $request->user(),
             ],
+            'flash' => $request->session()->get('flash'),
+            
         ];
     }
 }
