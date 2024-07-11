a144f6d5fecbaa7f0261956269fde11b8ca2a1ba
sa-kevin <kevin@s-arcana.co.jp>
2024-07-11 18:00:20 +0900

    add casts

--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -10,6 +10,11 @@ class Loan extends Model
     use HasFactory;
 
     protected $fillable = ['user_id', 'book_id', 'loaned_at', 'due_date', 'returned_at'];
+    protected $casts = [
+        'loaned_at' => 'datetime',
+        'due_date' => 'datetime',
+        'returned_at' => 'datetime',
+    ];
 
     public function user()
     {
