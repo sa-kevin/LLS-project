019ff5ae647964b8f947a3184a134a941001e73a
sa-kevin <kevin@s-arcana.co.jp>
2024-07-12 18:00:20 +0900

    fix: modify to send 1 mail for same return date

--- app/Notifications/BookReturnReminder.php
+++ app/Notifications/BookReturnReminder.php
@@ -7,15 +7,16 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\Collection;
 
 class BookReturnReminder extends Notification
 {
     use Queueable;
-    protected $loan;
+    protected $loans;
 
-    public function __construct(Loan $loan)
+    public function __construct(Collection $loans)
     {
-        $this->loan = $loan;
+        $this->loans = $loans;
     }
 
     
@@ -27,13 +28,19 @@ class BookReturnReminder extends Notification
     
     public function toMail(object $notifiable): MailMessage
     {
-        return (new MailMessage)
-                    ->subject('Reminder: Book Due for Return')
-                    ->line('This is a friendly reminder that the book you borrowed is due for return.')
-                    ->line('Book: ' . $this->loan->book->title)
-                    ->line('Due Date: ' . $this->loan->due_date->format('F j, Y'))
-                    ->action('View My Loans', url('/dashboard'))
-                    ->line('Thank you for using Tech Book!');
+        $mailMessage = (new MailMessage)
+            ->subject('Reminder: Book Due for Return')
+            ->line('This is a friendly reminder that the books you borrowed is due for return tomorrow.');
+        
+        foreach ($this->loans as $loan) {
+            $mailMessage->line("- {$loan->book->title}");
+        }
+
+        return $mailMessage
+                ->action('View My Loans', url('/dashboard'))
+                ->line('Thank you for using Tech Book!');
+
+        
     }
 
     /**
