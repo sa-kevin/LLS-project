5625fc4b434d46141c152688a00ffdb0e49ce88b
sa-kevin <kevin@s-arcana.co.jp>
2024-07-16 18:48:57 +0900

    fetest latest loaned book

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,14 +13,24 @@ class DashboardController extends Controller
     {
         $user = auth()->user();
         $wishlist = Wishlist::with('book')->where('user_id', $user->id)->get();
+        $totalLoans = Loan::where('user_id', $user->id)->count();
         $loans = Loan::with('book')
             ->where('user_id', $user->id)
             ->orderByRaw('ISNULL(returned_at) DESC, returned_at ASC, due_date ASC')
             ->get();
+        
+        $latestLoan = Loan::where('user_id', $user->id)
+            ->with('book')
+            ->latest('loaned_at')
+            ->first();
+        
+        $latestBookTitle = $latestLoan ? $latestLoan->book->title : 'No books loaned yet';
 
         return Inertia::render('Dashboard', [
             'wishlist' => $wishlist,
             'loans' => $loans,
+            'totalLoans' => $totalLoans,
+            'latestBookTitle' => $latestBookTitle,
             'flash' => session('flash', [],)
         ]);
     }
