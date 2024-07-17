04530f0c605307be94c8f9295b2505d515024c82
sa-kevin <kevin@s-arcana.co.jp>
2024-07-17 16:46:58 +0900

    add route for upload

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
 use App\Http\Controllers\BookController;
-use App\Http\Controllers\Dashboard;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\EmptyPageController;
 use App\Http\Controllers\LoanController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UploadController;
 use App\Http\Controllers\WaitingListController;
 use App\Http\Controllers\WishlistController;
-use App\Models\Wishlist;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -23,16 +23,22 @@ Route::get('/', function () {
 
 Route::middleware(['auth', 'verified'])->group(function (){
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/empty-page', [EmptyPageController::class, 'index'])->name('empty-page');
     
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::resource('books', BookController::class)->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
     Route::resource('waitinglists', WaitingListController::class);
     Route::resource('wishlists', WishlistController::class);
-    Route::resource('books', BookController::class);
+
     Route::resource('loans', LoanController::class);
     Route::post('/loans/{loan}/return', [LoanController::class, 'returnBook'])->name('loans.return');
-});
+    
+    Route::get('/upload', [UploadController::class, 'show'])->name('upload.show');
+    Route::post('/upload', [UploadController::class, 'uploadProcess'])->name('upload.process');
 
-require __DIR__.'/auth.php';
+    });
+    
+    require __DIR__.'/auth.php';
