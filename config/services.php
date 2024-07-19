1a2a9f3a9a848718483422cb8db2db7af4cd594b
sa-kevin <kevin@s-arcana.co.jp>
2024-07-19 16:52:02 +0900

    google api key

--- config/services.php
+++ config/services.php
@@ -34,5 +34,8 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    'google_books' => [
+        'api_key' => env('GOOGLE_BOOKS_API_KEY'),
+    ],
 
 ];
